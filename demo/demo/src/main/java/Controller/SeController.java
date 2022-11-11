44cdef46d87b2967b90e6e88e8a094671d622b83
bbsjz <2254500366@qq.com>
2022-11-12 00:11:19 +0800

    无法访问接口

---
+++ demo/demo/src/main/java/Controller/SeController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@RestController
+public class SeController {
+    String url=ClassLoader.getSystemResource("")+"File";
+
+    @RequestMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping("/{id}")
+    public byte[] getSwiperPic(@PathVariable String id) throws IOException {
+        File file=new File(url+"/"+id);
+        FileInputStream fileInputStream=new FileInputStream(file);
+        byte[] bytes=new byte[fileInputStream.available()];
+        fileInputStream.read(bytes,0,fileInputStream.available());
+        return bytes;
+    }
+
+    @GetMapping("")
+    public String hello()
+    {
+        return "hello";
+    }
+}
