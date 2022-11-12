6a642c0da00fcdd4bd9738c65ba0ec4d16d423ad
bbsjz <2254500366@qq.com>
2022-11-12 11:58:00 +0800

    文件目录不正确

--- demo/demo/src/main/java/com/example/demo/Controller/SeController.java
+++ demo/demo/src/main/java/com/example/demo/Controller/SeController.java
@@ -1,6 +1,11 @@
 package com.example.demo.Controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +15,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+@Api("硒施用户端")
 @RestController
 @RequestMapping("/selenium")
 public class SeController {
     String url=ClassLoader.getSystemResource("")+"File";
 
-    @RequestMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    @ApiOperation("获取开屏大图，传入参数为图片id，共三张，分别为1，2，3")
     @GetMapping("/{id}")
-    public byte[] getSwiperPic(@PathVariable String id) throws IOException {
-        File file=new File(url+"/"+id);
+    public ResponseEntity<byte[]> getSwiperPic(@PathVariable String id) throws IOException {
+        File file=new File(url+"/"+id+".jpg");
         FileInputStream fileInputStream=new FileInputStream(file);
         byte[] bytes=new byte[fileInputStream.available()];
         fileInputStream.read(bytes,0,fileInputStream.available());
-        return bytes;
+        final HttpHeaders headers=new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<>(bytes,headers, HttpStatus.OK);
     }
 
 }
